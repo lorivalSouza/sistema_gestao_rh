3fc2fc0e4a3d38891ee5684fda7c8b1d13b6d068
lorivalSouza <lorivalnew@gmail.com>
2021-05-05 11:57:23 -0300

    Finalização hora extra

--- registro_hora_extra/urls.py
+++ registro_hora_extra/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import HoraExtraList
-    #,HoraExtraCreate
-    #, EmpresaEdit
+from .views import HoraExtraList, HoraExtraEdit, HoraExtraDelete, HoraExtraCreate
 
 urlpatterns = [
     path('', HoraExtraList.as_view(), name='list_hora_extra'),
-    #path('novo', HoraExtraCreate.as_view(), name='create_hora_extra'),
-    #path('editar/<int:pk>/',
-         #EmpresaEdit.as_view(), name='edit_empresa'),
+    path('novo', HoraExtraCreate.as_view(), name='create_registro_hora_extra'),
+    path('editar/<int:pk>/',
+         HoraExtraEdit.as_view(), name='update_hora_extra'),
+    path('deletar/<int:pk>/',
+         HoraExtraDelete.as_view(), name='delete_hora_extra'),
 ]
--- registro_hora_extra/views.py
+++ registro_hora_extra/views.py
@@ -1,11 +1,26 @@
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView, DeleteView, CreateView
 from .models import RegistroHoraExtra
 
 
 class HoraExtraList(ListView):
     model = RegistroHoraExtra
-    #fields = ['motivo', 'horas']
 
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return RegistroHoraExtra.objects.filter(funcionario__empresa=empresa_logada)
+
+
+class HoraExtraEdit(UpdateView):
+    model = RegistroHoraExtra
+    fields = ['motivo', 'funcionario', 'horas']
+
+
+class HoraExtraDelete(DeleteView):
+    model = RegistroHoraExtra
+    success_url = reverse_lazy('list_hora_extra')
+
+
+class HoraExtraCreate(CreateView):
+    model = RegistroHoraExtra
+    fields = ['motivo', 'funcionario', 'horas']
